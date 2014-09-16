2471427a61d2fb36e15148e9698be161faa85531
mlennon3@gmail.com <mlennon3@gmail.com>
2014-09-16 15:59:11 -0500

    Added tests for controller to use instagram service

---
+++ spec/services/instagram_service_spec.rb
@@ -0,0 +1,9 @@
+# require 'spec_helper'
+
+# describe InstagramService do
+#   it 'returns Grams' do
+#     expect(GramFactory).to receive(:make_grams).with(SampleInstagramResponses.instagram_response)
+
+#     InstagramService.get_grams_by_hashtag("#{ENV["HASHTAG"]}")
+#   end
+# end
