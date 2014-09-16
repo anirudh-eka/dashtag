222d9208adb2838df036fd582c8a4dac86fd6986
mlennon3@gmail.com <mlennon3@gmail.com>
2014-09-16 17:09:44 -0500

    Added gram model

--- app/services/instagram_service.rb
+++ app/services/instagram_service.rb
@@ -2,7 +2,7 @@ class InstagramService
 	def self.get_grams_by_hashtag(hashtag)
 	    instagram_client_id = ENV["INSTAGRAM_CLIENT_ID"]
 	    response = HTTParty.get("https://api.instagram.com/v1/tags/#{hashtag}/media/recent?client_id=#{instagram_client_id}")
-	    GramFactory.make_grams(response)
+	    GramFactory.make_grams(response.parsed_response)
 	    return response
   	end
 end
--- spec/services/instagram_service_spec.rb
+++ spec/services/instagram_service_spec.rb
@@ -1,9 +1,8 @@
-# require 'spec_helper'
+require 'spec_helper'
 
-# describe InstagramService do
-#   it 'returns Grams' do
-#     expect(GramFactory).to receive(:make_grams).with(SampleInstagramResponses.instagram_response)
-
-#     InstagramService.get_grams_by_hashtag("#{ENV["HASHTAG"]}")
-#   end
-# end
+describe InstagramService do
+  it 'returns Grams' do
+    expect(GramFactory).to receive(:make_grams).with(SampleInstagramResponses.instagram_response)
+    InstagramService.get_grams_by_hashtag("#{ENV["HASHTAG"]}")
+  end
+end
