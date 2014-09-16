db33b4edb502ce426145aa431d94816074f5eb6c
mlennon3@gmail.com <mlennon3@gmail.com>
2014-09-16 11:04:52 -0500

    Added instagram_service

---
+++ app/services/instagram_service.rb
@@ -0,0 +1,7 @@
+class InstagramService
+	def self.get_grams_by_hashtag(hashtag)
+	    instagram_client_id = ENV["INSTAGRAM_CLIENT_ID"]
+	    response = HTTParty.get("https://api.instagram.com/v1/tags/#{hashtag}/media/recent?client_id=#{instagram_client_id}")
+	    return response
+  	end
+end
