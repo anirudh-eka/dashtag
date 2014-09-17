0cf4505b7fffb279409f5d418e1f34ea79235773
adhulip <rudysback@gmail.com>
2014-09-17 18:28:09 -0500

    refactor duplicate logic into private method

--- app/controllers/feed_controller.rb
+++ app/controllers/feed_controller.rb
@@ -3,24 +3,29 @@ class FeedController < ApplicationController
 	def index
     respond_to do |format|
       format.html do 
-        TweetService.get_tweets_by_hashtag(ENV["HASHTAG"])
-        InstagramService.get_grams_by_hashtag(ENV["HASHTAG"])
-        tweets = Tweet.all
-        grams = Gram.all
+        update_tweets_and_grams_with_hashtag ENV["HASHTAG"]
 
-        @posts = (tweets + grams).sort_by { |post| post.created_at }
-        @posts.reverse!
+        get_all_posts_in_desc_order
+
         render "index"
       end
 
       format.json do 
-        tweets = Tweet.all
-        grams = Gram.all
-        @posts = (tweets + grams).sort_by { |post| post.created_at }
-        @posts.reverse!
+        get_all_posts_in_desc_order
         render :json => @posts
       end
     end
-
   end
+
+  private
+
+    def get_all_posts_in_desc_order
+      @posts = (Tweet.all + Gram.all).sort_by { |post| post.created_at }
+      @posts.reverse!
+    end
+
+    def update_tweets_and_grams_with_hashtag(hashtag)
+      TweetService.get_tweets_by_hashtag(hashtag)
+      InstagramService.get_grams_by_hashtag(hashtag)
+    end
 end
