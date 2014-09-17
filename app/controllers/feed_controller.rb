ae280a8072d61a5c5552a1d365d92995b98ee302
adhulip <rudysback@gmail.com>
2014-09-17 18:19:28 -0500

    take out unnecessary instance variables

--- app/controllers/feed_controller.rb
+++ app/controllers/feed_controller.rb
@@ -5,18 +5,18 @@ class FeedController < ApplicationController
       format.html do 
         TweetService.get_tweets_by_hashtag(ENV["HASHTAG"])
         InstagramService.get_grams_by_hashtag(ENV["HASHTAG"])
-        @tweets = Tweet.order(created_at: :desc)
-        @grams = Gram.order(created_at: :desc)
+        tweets = Tweet.order(created_at: :desc)
+        grams = Gram.order(created_at: :desc)
 
-        @posts = (@tweets + @grams).sort_by { |post| post.created_at }
+        @posts = (tweets + grams).sort_by { |post| post.created_at }
         @posts.reverse!
         render "index"
       end
 
       format.json do 
-        @tweets = Tweet.order(created_at: :desc)
-        @grams = Gram.order(created_at: :desc)
-        @posts = (@tweets + @grams).sort_by { |post| post.created_at }
+        tweets = Tweet.order(created_at: :desc)
+        grams = Gram.order(created_at: :desc)
+        @posts = (tweets + grams).sort_by { |post| post.created_at }
         @posts.reverse!
         render :json => @posts
       end
