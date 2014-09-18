0034594b0c254c9f345d84ee13f5f61bbc87a09e
adhulip <rudysback@gmail.com>
2014-09-18 10:30:19 -0500

    create a post model'

--- app/controllers/feed_controller.rb
+++ app/controllers/feed_controller.rb
@@ -5,27 +5,22 @@ class FeedController < ApplicationController
       format.html do 
         update_tweets_and_grams_with_hashtag ENV["HASHTAG"]
 
-        get_all_posts_in_desc_order
+        @posts = Post.all
 
         render "index"
       end
 
       format.json do 
-        get_all_posts_in_desc_order
+        @posts = Post.all
         render :json => @posts
       end
     end
   end
 
   private
-
-    def get_all_posts_in_desc_order
-      @posts = (Tweet.all + Gram.all).sort_by { |post| post.created_at }
-      @posts.reverse!
-    end
-
-    def update_tweets_and_grams_with_hashtag(hashtag)
-      TweetService.get_tweets_by_hashtag(hashtag)
-      InstagramService.get_grams_by_hashtag(hashtag)
-    end
+  
+  def update_tweets_and_grams_with_hashtag(hashtag)
+    TweetService.get_tweets_by_hashtag(hashtag)
+    InstagramService.get_grams_by_hashtag(hashtag)
+  end
 end
