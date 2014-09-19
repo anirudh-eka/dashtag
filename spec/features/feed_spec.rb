f4da8c5b91095299939d714ca263fca2c5b43704
adhulip <rudysback@gmail.com>
2014-09-19 11:42:08 -0500

    use singleton solution for auto update

--- spec/features/feed_spec.rb
+++ spec/features/feed_spec.rb
@@ -4,12 +4,9 @@ require 'rake'
 describe 'home' do
   let(:profile_image) {"http://a0.twimg.com/profile_images/447958234/Lichtenstein_normal.jpg"}
   let(:media_image) {"https://pbs.twimg.com/media/BoqqU1wIMAAr_zO.jpg"}
-	it 'should display title' do
-		visit '/'
-		page.should have_content("##{ENV["HASHTAG"]}")
-	end
 
   it 'should display single tweets details' do
+    sleep 5.2
     visit '/'
 
     page.should have_content("Thee Namaste Nerdz. ##{ENV["HASHTAG"]}")
@@ -37,6 +34,7 @@ describe 'home' do
   end
 
   it 'should not have several swear words' do
+    sleep 5.2
     stub_request(:get, "https://api.twitter.com/1.1/search/tweets.json?q=%23#{ENV["HASHTAG"]}").
       with(headers: {"Authorization"=>/Bearer .+/}).
       to_return( {:status => 200, :body => SampleTweetResponses.tweets_with_censored_words.to_json, :headers => {'content-type' => 'application/json'} })
--- spec/services/tweet_service_spec.rb
+++ spec/services/tweet_service_spec.rb
@@ -2,8 +2,8 @@ require 'spec_helper'
 
 describe TweetService do
   it 'returns Tweets' do
-    expected_tweets = double("tweets")
     expect(TweetFactory).to receive(:make_tweets).with(SampleTweetResponses.tweet_response)
-    TweetService.get_tweets_by_hashtag("#{ENV["HASHTAG"]}")
+    sleep 5.2
+    TweetService.instance.get_tweets_by_hashtag("#{ENV["HASHTAG"]}")
   end
 end
