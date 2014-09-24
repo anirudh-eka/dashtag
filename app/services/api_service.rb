1d2e7d63606810edcc6fd185c96be49cc9a970e8
jpramos <justin.ramos21@gmail.com>
2014-09-24 17:08:27 -0500

    refactor duplicate logic in tweet_service and instagram_serve to api_service and delete them

---
+++ app/services/api_service.rb
@@ -0,0 +1,50 @@
+require 'singleton'
+
+class APIService
+  include Singleton
+  attr_reader :last_update
+
+  def initialize
+    @last_update = Time.new(1720)
+  end
+
+  def get_posts(hashtag)
+    rate_to_hit_api = ENV["API_Rate"] ? ENV["API_Rate"].to_f : 15
+    if (Time.now - @last_update > rate_to_hit_api)
+      @last_update = Time.now
+
+      pull_instagram_posts_and_parse(hashtag)
+      pull_twitter_posts_and_parse(hashtag)
+    end
+  end
+
+private
+
+	def pull_instagram_posts_and_parse(hashtag)
+    instagram_client_id = ENV["INSTAGRAM_CLIENT_ID"]
+    response = HTTParty.get("https://api.instagram.com/v1/tags/#{hashtag}/media/recent?client_id=#{instagram_client_id}")
+
+    GramFactory.make_grams(response.parsed_response)
+	end
+
+  def pull_twitter_posts_and_parse(hashtag)
+    response = HTTParty.get("https://api.twitter.com/1.1/search/tweets.json?q=%23#{hashtag}",
+    :headers => { "Authorization" => "Bearer #{twitter_bearer_token}",
+      "User-Agent" => "#NAAwayDay Feed v1.0"})
+    TweetFactory.make_tweets(response.parsed_response)
+  end
+
+  def twitter_bearer_token
+    authorization_key = Base64.encode64(ENV["TWITTER_BEARER_CREDENTIALS"]).gsub("\n","")
+    
+    resp = HTTParty.post('https://api.twitter.com/oauth2/token',
+      :headers => { 
+        "Authorization" => "Basic #{authorization_key}",
+        "User-Agent" => "Hashtag Displayer Feed v1.0",
+        "Content-Type" => "application/x-www-form-urlencoded;charset=UTF-8"
+        },
+      body: {"grant_type" => "client_credentials"}
+    )
+    resp["access_token"]
+  end
+end
