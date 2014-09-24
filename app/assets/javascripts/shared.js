a1503b41318d33966cb49d6fd8fd2b7e64e369f9
mlennon3@gmail.com <mlennon3@gmail.com>
2014-09-24 16:55:29 -0500

    Special characters are now unescaped

--- app/assets/javascripts/shared.js
+++ app/assets/javascripts/shared.js
@@ -4,7 +4,7 @@ var renderTweets = function (tweet) {
   var tweetContainer = posts_list.find(".tweet-container").last()
 
   tweetContainer.append("<section class='tweet-text'></section>");
-  tweetContainer.find(".tweet-text").text(tweet.text);
+  tweetContainer.find(".tweet-text").text(unescapeHtml(tweet.text));
 
   tweetContainer.append("<section class='tweet-username'></section>");
   tweetContainer.find(".tweet-username").html("<img src='" + tweet.profile_image_url + "' class='avatar' /> @" + tweet.screen_name);  
@@ -16,6 +16,10 @@ var renderTweets = function (tweet) {
 
   tweetContainer.append("<section class='tweet-created-at'><i class='fa fa-2x fa-"+tweet.source+"'></i>"+tweet.created_at_formatted+"</section>");
   return tweetContainer;
+}
+
+function unescapeHtml(safe) {
+    return $('<div />').html(safe).text();
 }
 
 var create_post_content = function(data) {
