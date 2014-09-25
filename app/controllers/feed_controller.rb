506db75556b6a0f51c6be4a3e5eb6943ab06c7a3
jpramos <justin.ramos21@gmail.com>
2014-09-25 17:39:33 -0500

    Refactor logic out of Feed Controller and into API service and Post Model

--- app/controllers/feed_controller.rb
+++ app/controllers/feed_controller.rb
@@ -1,18 +1,20 @@
+require 'pry'
 class FeedController < ApplicationController
 
-	def index
+  @number_of_posts_in_page = 50
+  def index
     respond_to do |format|
-      format.html do 
+      format.html do
         update_tweets_and_grams_with_hashtag ENV["HASHTAG"]
 
-        @posts = sort_by_date.page(params[:page]).per(50)
+        @posts = Post.sort_by_time_of_post.page(params[:page]).per(50)
 
         render "index"
       end
 
       format.json do
-        if @posts = get_new_posts(ENV["HASHTAG"])
-          
+        @posts = Post.get_new_posts(ENV["HASHTAG"])
+        if @posts
           render json: @posts
         else
           render json: @posts, status: :not_modified
@@ -22,51 +24,10 @@ class FeedController < ApplicationController
   end
 
   def get_next_page
-    number_of_posts_in_page = 50
     requested_page = params[:last_page_requested].to_i+1
 
-    @posts = sort_by_date.page(requested_page).per(number_of_posts_in_page)
+    @posts = sort_by_date.page(requested_page).per(@number_of_posts_in_page)
     render json: @posts
-  end
-
-  ######### service stuff
-  def pull_new_posts(hashtag)
-    @old_last_update = APIService.instance.last_update
-    APIService.instance.get_posts(hashtag)
-    @new_last_update = APIService.instance.last_update
-  end
-
-
-  def did_service_update?
-    @new_last_update > @old_last_update
-  end
-
-  def new_last_update
-    @new_last_update
-  end
-
-  #####Model stuff
-
-  def get_new_posts(hashtag)
-    pull_new_posts(hashtag)
-    if did_service_update?
-      Post.order(time_of_post: :desc).select { |post| is_post_from_last_pull?(post) }
-    else
-      nil
-    end
-  end
-
-  def is_post_from_last_pull?(post)
-    post.created_at > new_last_update
-  end
-
-
-
-#######
-
-
-  def sort_by_date
-    Post.order(time_of_post: :desc)
   end
 
   private
