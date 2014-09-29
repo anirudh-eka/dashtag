1785aa0b57187c30758627d19bcd43077d72fc78
adhulip <rudysback@gmail.com>
2014-09-29 11:18:58 -0500

    take out post creation responsibility entirely out of the parsers

--- app/parsers/gram_parser.rb
+++ app/parsers/gram_parser.rb
@@ -1,34 +1,5 @@
 class GramParser
 
-  def self.make_grams(parsed_response)
-    parsed_response["data"].each do |gram|
-      
-      text = (gram["caption"] && gram["caption"]["text"]) || String.new
-      
-      
-      return if text && text.match(/.*(#{ENV["CENSORED_WORDS"]}).*/i)   
-
-      unless gram["images"].nil?
-        unless gram["images"]["standard_resolution"].nil?
-            media_url = gram["images"]["standard_resolution"]["url"]
-        end
-      end
-      
-      screen_name = gram["user"]["username"]
-      profile_image_url = gram["user"]["profile_picture"]
-      created_at = DateTime.strptime(gram["created_time"], "%s")
-
-        Post.create(
-          source: "instagram",
-          text: text, 
-          screen_name: screen_name,
-          media_url: media_url,
-          profile_image_url: profile_image_url,
-          time_of_post: created_at
-          )
-    end
-  end
-
   def self.parse(response)
     parsed_response = []
     response["data"].each do |gram|
@@ -36,26 +7,27 @@ class GramParser
       text = (gram["caption"] && gram["caption"]["text"]) || String.new
       
       
-      return if text && text.match(/.*(#{ENV["CENSORED_WORDS"]}).*/i)   
+      unless text && text.match(/.*(#{ENV["CENSORED_WORDS"]}).*/i)   
 
-      unless gram["images"].nil?
-        unless gram["images"]["standard_resolution"].nil?
-            media_url = gram["images"]["standard_resolution"]["url"]
+        unless gram["images"].nil?
+          unless gram["images"]["standard_resolution"].nil?
+              media_url = gram["images"]["standard_resolution"]["url"]
+          end
         end
-      end
-      
-      screen_name = gram["user"]["username"]
-      profile_image_url = gram["user"]["profile_picture"]
-      created_at = DateTime.strptime(gram["created_time"], "%s")
+        
+        screen_name = gram["user"]["username"]
+        profile_image_url = gram["user"]["profile_picture"]
+        created_at = DateTime.strptime(gram["created_time"], "%s")
 
-      parsed_response << {
-          source: "instagram",
-          text: text, 
-          screen_name: screen_name,
-          media_url: media_url,
-          profile_image_url: profile_image_url,
-          time_of_post: created_at
-          }
+        parsed_response << {
+            source: "instagram",
+            text: text, 
+            screen_name: screen_name,
+            media_url: media_url,
+            profile_image_url: profile_image_url,
+            time_of_post: created_at
+            }
+      end
     end
     return parsed_response
   end
