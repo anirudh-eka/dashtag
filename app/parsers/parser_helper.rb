abfb814c00cf5ec285a3ae106645f3ef1a7fc8ee
adhulip <rudysback@gmail.com>
2014-10-05 09:36:01 -0500

    modify censoring to be optional by no requiring the key to be listed in the environment

--- app/parsers/parser_helper.rb
+++ app/parsers/parser_helper.rb
@@ -1,11 +1,11 @@
 module ParserHelper
   def self.text_has_censored_words(text)
-  	return false if ENV["CENSORED_WORDS"] == ""
+  	return false if ENV["CENSORED_WORDS"] == "" || ENV["CENSORED_WORDS"] == nil
     (text && text.match(/.*(#{ENV["CENSORED_WORDS"]}).*/i))
   end
 
   def self.user_is_censored(screen_name)
-  	return false if ENV["CENSORED_USERS"] == ""
+  	return false if ENV["CENSORED_USERS"] == "" || ENV["CENSORED_USERS"] == nil
     (screen_name && screen_name.match(/.*(#{ENV["CENSORED_USERS"]}).*/i))   
   end
 end
