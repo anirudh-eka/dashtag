50cd689aa189949345d81c681ca3db8a2288f5fb
ParkDan <dpinch86@gmail.com>
2014-11-10 11:59:26 -0600

    updated autoupdate

--- spec/factories/posts.rb
+++ spec/factories/posts.rb
@@ -1,12 +1,16 @@
 FactoryGirl.define do
 	factory :post do
 		screen_name "cassius_clay"
-    	profile_image_url "stuf.com"
-    	created_at Time.now - 2
-    	time_of_post Time.now - 5
-    	source "twitter"
-    	media_url "some media url"
-    	text "float like a butterfly"
-    	post_id "12345"
+  	profile_image_url "stuf.com"
+  	created_at Time.now - 2
+  	time_of_post Time.now - 5
+  	source "twitter"
+  	media_url "some media url"
+  	text "float like a butterfly"
+  	post_id "12345"
+
+    factory :random_post do
+      screen_name {Faker::Lorem.words.join("_")}
+    end
 	end
 end
