bdb7750ea2a09c54909c1b6fea62bb62808f2066
ParkDan <dpinch86@gmail.com>
2014-11-11 09:21:48 -0600

    removed commeneted code

--- app/assets/javascripts/services/ajaxService.js
+++ app/assets/javascripts/services/ajaxService.js
@@ -1,29 +1,6 @@
 
 var ajaxService = {
   last_update_time: Date.now(),
-  // setup: function(){
-  //   var self = this;
-  //   window.setInterval(function(){
-  //     $.ajax({
-  //       type: "GET",
-  //       url: "/",
-  //       data: {
-  //       "last_update_time": self.last_update_time
-  //           },
-  //       contentType: "application/json; charset=utf-8",
-  //       ifModified: true,
-  //       dataType: "json",
-  //       success: function(response, status){
-  //         console.log(status)
-  //         if(status != "notmodified") {
-  //           $(self).trigger("new-posts", [response]);
-  //           console.log(response)
-  //           self.last_update_time = Date.now();
-  //         }
-  //       }
-  //     });
-  //   }, 5000);
-  // },
 
   setup: function(){
     var self = this;
@@ -38,10 +15,8 @@ var ajaxService = {
         ifModified: true,
         dataType: "json",
         success: function(response, status){
-          console.log(status)
           if(status != "notmodified") {
             $(self).trigger("new-posts", [response]);
-            console.log(response)
             self.last_update_time = Date.now();
           }
         },
