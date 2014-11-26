f735af54944b2a8262831485bca0fc43d01dad75
Brandon Teng <bteng22@gmail.com>
2014-11-26 10:46:01 -0600

    jasmine test : dateHelper should replace initially loaded timestamps

--- spec/javascripts/helpers/dateHelperSpec.js
+++ spec/javascripts/helpers/dateHelperSpec.js
@@ -1,20 +1,23 @@
 describe('dateHelper', function() {
 	var dateHelper = dashtag.dateHelper();
 
+	// TODO : first test may not be needed since functionality being tested by
+	// "should replace initially loaded timestamps"
 	it("should format date to local timezone", function() {
 		var timestampDate = new Date("2014", 10, "25", "20", "30", "34");
 
 		var formattedDate = dateHelper.formatDateToLocalTimezone(timestampDate);
 
-		expect(formattedDate).toEqual("Tue Nov 25 8:30:34 PM");
+		expect(formattedDate).toContain("Tue Nov 25 8:30:34 PM");
 	});
 
-	// it("replace initially loaded timestamps", function() {
- //    var timeOfPost = '<span class=​"time-of-post">​ 2014-11-25 21:27:04 UTC ​</span>​'
- //    var $jQueryObject = $($.parseHTML(timeOfPost));
- //    console.log($jQueryObject);
+	it("should replace initially loaded timestamps", function() {
+    var timeOfPost = "<span class='time-of-post'> 2014-11-26 16:10:15 UTC </span>"
+    var $jQueryObject = $($.parseHTML(timeOfPost));
 
- //    var expected = dateHelper.replaceInitiallyLoadedTimestamps($jQueryObject);
+    dateHelper.replaceInitiallyLoadedTimestamps($jQueryObject);
 
-	// });
+    expect($jQueryObject.text()).toContain("Wed Nov 26 4:10:15 PM")
+	});
+
 });
