88623f2eb1e133d1cf28ce78074e0a433527ccd2
ParkDan <dpinch86@gmail.com>
2014-12-01 10:48:49 -0600

    added jasmine specs

--- spec/javascripts/helpers/renderPostHelperSpec.js
+++ spec/javascripts/helpers/renderPostHelperSpec.js
@@ -8,5 +8,19 @@ describe('renderPostHelper', function() {
     var testPostViewModels = renderPostHelper.createPostContent(testPostModels);
 
 		expect(testPostViewModels[0]).toHaveClass('post-container item');
-		});
+		expect(testPostViewModels[1]).toHaveClass('post-container item');
+
+		expect(testPostViewModels[0].find('.post-username')).toContainText("sydneyreuille");
+		expect(testPostViewModels[1].find('.post-username')).toContainText("noavisar");
+
+		expect(testPostViewModels[0].find('.post-text')).toContainText("Lunch time with the team #dance #reunited #power");
+		expect(testPostViewModels[1].find('.post-text')).toContainText("#Dance #Movement #Life");
+
+		expect(testPostViewModels[0]).toHaveClass('post-color-1');
+		expect(testPostViewModels[1]).toHaveClass('post-color-2');
+
+		expect(testPostViewModels[0].find('.post-created-at')).toContainHtml('<a href="https://instagram.com/p/v3-8inO00j" target="_blank"><i class="fa fa-2x fa-instagram"></i><span class="time-of-post">Wed Nov 26 1:30:48 PM</span></a>')
+		expect(testPostViewModels[1].find('.post-created-at')).toContainHtml('<a href="https://instagram.com/p/v3-8KMDAPR" target="_blank"><i class="fa fa-2x fa-instagram"></i><span class="time-of-post">Wed Nov 26 1:30:45 PM</span></a>')
+
+	});
 });
